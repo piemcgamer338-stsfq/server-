04fdebb4871985eb8320d3ac5ec80be36928ef06
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-08-02 14:45:09 +0530

    Add ribbon image URL to embeds after descriptions

--- embeds.js
+++ embeds.js
@@ -24,7 +24,8 @@ module.exports = {
 
 https://discord.com/terms
 https://discord.com/guidelines`
-            );
+            )
+            .setImage("https://media.discordapp.net/attachments/1359151586835693662/1440892681634054164/unknown.png?ex=6a6ffde1&is=6a6eac61&hm=ad54b064cb4c20903e68ad2ddb830982dda473790d0e5cf8d5bd4b714e2e95d7&=&format=webp&quality=lossless");
 
     },
 
@@ -39,12 +40,13 @@ https://discord.com/guidelines`
 
 <a:dot:1528726500075900968> Must have **"Supporter" Role**. You can get it by putting your status .gg/6xy
 
-<a:dot:1528726500075900968> Must have **"VIP" Role** <:VIP:1533026174526165043>. You can get it by dragging **Miku Giveaways** on top of your server list and ask for role in ⁠unknown after dragging our server on top of your server list.
+<a:dot:1528726500075900968> Must have **"VIP" Role** <:VIP:1533026174526165043>. You can get it by dragging **Miku Giveaways** on top of your server list and ask for role in ⁠unknown after dragg[...]
 
 <a:dot:1528726500075900968> Must be active in <#1532910791546638471> and follow our server pings.
 
 <a:dot:1528726500075900968> Must have **"6xy" Tag** <a:rb1:1530379253735231562>`
-            );
+            )
+            .setImage("https://media.discordapp.net/attachments/1359151586835693662/1440892681634054164/unknown.png?ex=6a6ffde1&is=6a6eac61&hm=ad54b064cb4c20903e68ad2ddb830982dda473790d0e5cf8d5bd4b714e2e95d7&=&format=webp&quality=lossless");
 
     },
 
@@ -60,7 +62,8 @@ https://discord.com/guidelines`
 **1x** Boost : 2 Week Nitro <:nitro:1533026966515613828> & 0.20 <:Ltc:1527472200641286244>
 
 **2x** Boost : 2 Week Nitro <:nitro:1533026966515613828> & 0.50 <:Ltc:1527472200641286244>`
-            );
+            )
+            .setImage("https://media.discordapp.net/attachments/1359151586835693662/1440892681634054164/unknown.png?ex=6a6ffde1&is=6a6eac61&hm=ad54b064cb4c20903e68ad2ddb830982dda473790d0e5cf8d5bd4b714e2e95d7&=&format=webp&quality=lossless");
 
     }
 
